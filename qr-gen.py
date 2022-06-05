5025708b773a1192c4069d161728f8f3eab3cbad
Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-06-05 11:09:50 +0200

    Update qr-gen.py
    Color

--- qr-gen.py
+++ qr-gen.py
@@ -1,5 +1,9 @@
+Class color:
+   UNDERLINE = '\033[4m'
+
 import pyqrcode
 import io
+From colorama import init
 
 link = input("URL: ")
 url = pyqrcode.create(link)
@@ -7,4 +11,6 @@ url = pyqrcode.create(link)
 url.svg('output.svg', scale=4)
 buffer = io.Bytes.IO()
 url.svg(buffer)
-print("Done!")
+print(f' ')
+print(color.UNDERLINE + f 'Done!')
+print(f' ')
